d2d47c366c4a0f6a2f481d63bb749e3ced149314
Amy Young <youngamy1223@gmail.com>
2022-06-04 23:37:43 -0600

    finish form functionality for template

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -9,7 +9,8 @@ class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      formVisibleOnPage: false,
+      mainTicketList: []
     };
   }
 
@@ -17,16 +18,23 @@ class TicketControl extends React.Component {
     this.setState(prevState => ({ formVisibleOnPage: !prevState.formVisibleOnPage }));
   }
 
+  handleAddingNewTicketToList = (newTicket) => {
+    const newMainTicketList = this.state.mainTicketList.concat(newTicket);
+    this.setState({ mainTicketList: newMainTicketList, formVisibleOnPage: false });
+  }
+
   render() {
     let currentlyVisibleState = null;
     let buttonText = null;
     if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewTicketForm />;
+      currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />;
       buttonText = "Return to TicketList";
     } else {
-      currentlyVisibleState = <TicketList />
+      currentlyVisibleState = <TicketList ticketList={this.state.mainTicketList}/>
       buttonText = "Add Ticket";
     }
+    console.log(currentlyVisibleState);
+    console.log(buttonText);
     return (
       <React.Fragment>
         {currentlyVisibleState}
--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,37 +1,25 @@
 import React from "react";
 import Ticket from "./Ticket";
+import PropTypes from "prop-types";
 
-const mainTicketList = [
-  {
-    names: 'Thato and Haley',
-    location: '3A',
-    issue: 'Firebase won\'t save record. Halp.'
-  },
-  {
-    names: 'Sleater and Kinney',
-    location: '4B',
-    issue: 'Prop types are throwing an error.'
-  },
-  {
-    names: 'Imani & Jacob',
-    location: '9F',
-    issue: 'Child component isn\'t rendering.'
-  }  
-];
-
-function TicketList() {
-  
+function TicketList(props) { // Add props as parameter.
   return (
-  <React.Fragment>
-    <hr/>
-    {mainTicketList.map((ticket, index) =>
-      <Ticket names={ticket.names}
-        location={ticket.location}
-        issue={ticket.issue}
-        key={index}/>
-    )}
-  </React.Fragment>
-);}
+    <React.Fragment>
+      <hr />
+      {props.ticketList.map((ticket, index) => // Loop through the list passed down from TicketControl.
+        <Ticket names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue}
+          key={index} />
+      )}
+    </React.Fragment>
+  );
+}
+
+// Add propTypes for ticketList
+TicketList.propTypes = {
+  ticketList: PropTypes.array
+};
 
 export default TicketList;
   
